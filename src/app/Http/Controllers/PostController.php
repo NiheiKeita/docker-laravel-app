954025f9f9320a3cb4649c03815065f338a8b34c
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-03-08 00:27:07 +0900

    修正

--- src/app/Http/Controllers/PostController.php
+++ src/app/Http/Controllers/PostController.php
@@ -15,30 +15,36 @@ class PostController extends Controller
     public function create()
     {
         //
+        return "create";
     }
 
     public function store(Request $request)
     {
         //
+        return "store";
     }
 
     public function show($id)
     {
         //
+        return "show";
     }
 
     public function edit($id)
     {
         //
+        return "edit";
     }
 
     public function update(Request $request, $id)
     {
         //
+        return "update";
     }
 
     public function destroy($id)
     {
         //
+        return "destroy";
     }
 }
